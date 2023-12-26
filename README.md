e475346a5d533f4de0489a6cf2b47f739b3bbf6a
Sudharsan <sudharsan.srinivasan@microsoft.com>
2023-12-26 15:07:26 -0800

    Day1 Part1

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Advent of Code
 Advent of code posts questions here: https://adventofcode.com/.
-No reason to choose a particular programming language for a question unless stated otherwise :)
+No reason to choose a particular programming language for a question unless stated otherwise.
+
+# Folder Structure
+Each day has its own folder. Typically there are two parts in each day. Each part has its own folder. 
+
+The question for each day is in day/question.txt.
+Input for each part is in day/part/input.txt.
---
+++ com/srinisudharsan/advent2023/day1/part1/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        long sum = 0;
+        String line;
+        try {
+            line = reader.readLine();
+            while(line != null){
+                System.out.println("Line: " + line);
+                char[] chars = line.toCharArray();
+                int currVal = 0;
+                for(int i = 0; i < chars.length; i++){
+                    // we know it is only two digits
+                    if(chars[i] >= '0' && chars[i] <= '9'){
+                        currVal = chars[i] - '0';
+                        break;
+                    }
+                }
+                currVal *= 10;
+
+                for(int i = chars.length - 1; i >=0 ; i--){
+                    // we know it is only two digits
+                    if(chars[i] >= '0' && chars[i] <= '9'){
+                        currVal += (chars[i] - '0');
+                        break;
+                    }
+                }
+                sum += currVal;
+                System.out.println("CurrVal: " + currVal);
+                System.out.println("Sum: " + sum);
+                line = reader.readLine();
+            }
+            System.out.println("Result: " + sum);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            reader.close();
+        }
+    }
+}
