0bb4b50eb5a825d20a39c82b3195a67bf6046d09
Sudharsan <sudharsan.srinivasan@microsoft.com>
2023-12-28 00:03:14 -0800

    Test Game2

---
+++ src/main/java/com/srinisudharsan/aoc2023/day2/part1/MinCubeProductCalculator.java
@@ -0,0 +1,29 @@
+package com.srinisudharsan.aoc2023.day2.part1;
+
+public class MinCubeProductCalculator{
+    public static int computeMinCubeProduct(String game){
+        String games = game.substring(game.indexOf(':')+2);
+        int red = 0;
+        int blue = 0;
+        int green = 0;
+        for(String subGame : games.split(";")){
+            for(String color : subGame.trim().split(",")){
+                String[] vals = color.trim().split(" ");
+                if(vals != null && vals.length == 2){
+                    switch (vals[1].toLowerCase()){
+                        case "red":
+                            red = Math.max(red, Integer.parseInt(vals[0].trim()));
+                            break;
+                        case "green":
+                            green = Math.max(green, Integer.parseInt(vals[0].trim()));
+                            break;
+                        case "blue":
+                            blue = Math.max(blue, Integer.parseInt(vals[0].trim()));
+                            break;
+                    }
+                }
+            }
+        }
+        return red * green * blue;
+    }
+}
